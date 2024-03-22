ee4c2b02afebf73bf76df506c2292995ea09c158
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-03-22 18:07:53 +0100

    Trying #1 in 0x0A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # Using Puppet, install flask from pip3.
-package {
-    'flask':
-    command => '/usr/bin/pip3 install -q flask==2.1.0 --break-system-packages'
+package { 'flask':
+    ensure  => 'installed',
+    command => '/usr/bin/pip3 install flask==2.1.0 --break-system-packages'
   }
