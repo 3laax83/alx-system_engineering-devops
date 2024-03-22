cc8c4dd6a7bbec3aaa306459e0d4dca96ef68bf5
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-03-22 18:01:51 +0100

    A new try for #1 - 0x0A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,5 @@
 # Using Puppet, install flask from pip3.
 package {
     'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+    command => '/usr/bin/pip3 install -q flask==2.1.0 --break-system-packages'
   }
