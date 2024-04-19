8e98bfad07dc7a551ec2a96f5d3d77bd41501dcb
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-04-19 07:53:34 +0100

    Add fixes for all files in 0x0B

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,19 @@
+# Configuration file for clients
+
+include stdlib
+
+File_line {
+    'Turn off passwd auth':
+    ensure    => present,
+    path      => '/etc/ssh/ssh_config'
+    line      => 'PasswordAuthentication no',
+    replace   => true,
+  }
+
+File_line {
+    'Declare identity file':
+    ensure    => present,
+    path      => '/etc/ssh/sshd_config'
+    line      => 'IdentifyFile ~/.ssh/school'
+    replace   => true,
+  }
