f2d20adf32ccb46fa5b25efead2108145ae9f29d
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-04 20:16:02 +0100

    edit #2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,25 @@
 # Automate the task #0 with puppet
 
 exec { 'update-apt':
-  command   => 'apt-get -y update',
+  command   => 'apt-get update',
   user      => 'root',
   provider  => 'shell',
   }
-->
+
 package { 'nginx':
   ensure    => present,
   provider  => 'apt',
 }
-->
+
 file_line { 'Add-HTTP-HEADER':
   ensure    => 'present',
   path      => '/etc/nginx/sites-available/default',
   after     => 'listen 80 default_server',
   line      => 'add_header X-Served-By $hostname',
   }
-->
+
 service { 'nginx':
   ensure    => 'running',
   enable    => true,
   require   => Package['nginx'],
-}
+  }
