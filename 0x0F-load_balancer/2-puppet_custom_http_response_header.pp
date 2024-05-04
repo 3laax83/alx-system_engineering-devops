f12653b739a015cc9bc3fa4c55fe161893845745
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-04 20:11:26 +0100

    0x0F - add file #2 - puppet implementation of #0

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,25 @@
+# Automate the task #0 with puppet
+
+exec { 'update-apt':
+  command   => 'apt-get -y update',
+  user      => 'root',
+  provider  => 'bash',
+  }
+
+package { 'nginx':
+  ensure    => present,
+  provider  => 'apt',
+}
+
+file_line { 'Add-HTTP-HEADER':
+  ensure    => 'present',
+  path      => '/etc/nginx/sites-available/default',
+  after     => 'listen 80 default_server',
+  line      => 'add_header X-Served-By $hostname',
+  }
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  require   => Package['nginx'],
+}
