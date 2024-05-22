b02ab4a9c6431aa24064fc6eced102a685d3cedf
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-22 15:36:34 +0100

    0x15 - add #3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python script to
+export data in the JSON format.
+
+Requirements:
+
+Records all tasks from all employees
+Format must be: { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ],
+"USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS}, ... ]}
+File name must be: todo_all_employees.json
+
+"""
+
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    RESTAPI = "https://jsonplaceholder.typicode.com/users/"
+    alldata = requests.get(RESTAPI)
+    usersAll = alldata.json()
+    usersDict = {}
+
+    for user in usersAll:
+        userID = user.get("id")
+        username = user.get("username")
+        url = RESTAPI + "{}".format(userID)
+        url = url + "/todos"
+        resp = requests.get(url)
+        tasks = resp.json()
+        usersDict[userID] = []
+        for task in tasks:
+            tasksComp = task.get("completed")
+            tasksTitle = task.get("title")
+            usersDict[userID].append(
+                {"task": tasksTitle, "completed": tasksComp, "username": username}
+            )
+        with open("todo_all_employees.json", "w") as f:
+            json.dump(usersDict, f)
